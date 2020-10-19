f60b908869b6e56ad28a72cab43050bad7ba89bb
Krimzin <email.krimzin@gmail.com>
2020-10-19 16:40:59 +1300

    Minor code cleanup

--- Simple-Bipods/hooks/Localization.lua
+++ Simple-Bipods/hooks/Localization.lua
@@ -1,14 +1,15 @@
 Hooks:Add("LocalizationManagerPostInit", "SimpleBipods_localization", function (self)
-	local lang_key = SystemInfo:language():key()
-	local lang_file_name = "english.txt"
-	for _, file_name in pairs(file.GetFiles(SimpleBipods.localization_path)) do
-		if Idstring(file_name:match("^(.*).txt$")):key() == lang_key then
-			lang_file_name = file_name
+	local path = SimpleBipods.localization_path
+	local key = SystemInfo:language():key()
+	local file_name = "english.txt"
+	for _, name in pairs(file.GetFiles(path)) do
+		if Idstring(name:match("^(.*).txt$")):key() == key then
+			file_name = name
 			break
 		end
 	end
-	local lang_path = SimpleBipods.localization_path .. lang_file_name
-	if io.file_is_readable(lang_path) then
-		self:load_localization_file(lang_path)
+	local path = path .. file_name
+	if io.file_is_readable(path) then
+		self:load_localization_file(path)
 	end
 end)
